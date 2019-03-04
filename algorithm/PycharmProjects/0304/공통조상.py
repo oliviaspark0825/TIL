5bbad85af9a5f0254f985d89ac5ad0d9004821bd
oliviaspark0825 <suhyunpark0825@gmail.com>
2019-03-05 06:41:35 +0900

    tree home

--- algorithm/PycharmProjects/0304/공통조상.py
+++ algorithm/PycharmProjects/0304/공통조상.py
@@ -1,17 +1,20 @@
-def ancestor(f1,f2): # 8, 13
+def ancestor(f1,f2):
     whole = []
     while f1 != 0:
-        whole.append(tree[f1-1][3])
+        whole.append(tree[f1][2])
         f1 = whole[-1]
-    whole.pop()
-    if
+    begin = tree[f2][2]
+    while begin:
+        if begin in whole:
+            return begin
+        begin = tree[begin][2]
 
-
-
-    if tree[i][3] == f2 or tree[i][3] == f2:
-        ans2 = i
-
-
+def preorder(V):
+    global cnt
+    if V != 0:
+        cnt += 1
+        preorder(tree[V][0])
+        preorder(tree[V][1])
 
 
 def printTree():
@@ -21,10 +24,11 @@ def printTree():
 import sys
 sys. stdin = open('조상.txt', 'r')
 T = int(input())
-for tc in range(1):
+for tc in range(T):
     V, E, f1,f2 = list(map(int,input().split()))
     tree = [[0 for _ in range(3)] for _ in range(V + 1)]
     temp = list(map(int,input().split()))
+    cnt = 0
     for i in range(E):  # 2개씩 받아서 먼저 들어오면 left 방에 넣고, 아니면 righ
         n1 = temp[i * 2]
         n2 = temp[i * 2 + 1]
@@ -33,5 +37,6 @@ for tc in range(1):
         else:
             tree[n1][1] = n2
         tree[n2][2] = n1
-printTree()
-ancestor(f1,f2)
+    same_root = ancestor(f1,f2)
+    preorder(same_root)
+    print("#{} {} {}".format(tc+1, same_root, cnt))
